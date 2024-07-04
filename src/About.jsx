cb5f17bc89000a0190f6227eb97ae9c2ddb20bf8
Vyshnav R <vyshnavr856@gmail.com>
2024-07-04 21:39:15 +0530

    created about section

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./styles/About.css"
+
+export default function About() {
+	return (
+		<div className="about-container">
+			<h1 className="about-heading">Hello 👋 I'm Vyshnav</h1>
+			<h3 className="about-subheading">a full-stack web developer</h3>
+			
+			<p className="intro-para">I am a passionate full-stack web-developer who loves to create fast and easy-to-use web apps. I'm currently studying computer science at VIT Vellore. </p>
+
+			<p className="intro-para-2">My love for web development began when I was first introduced to basic HTML during my first year of college, from where it evolved to learning and implementing different technologies to create fully-fledged web apps.</p>
+		</div>
+	)
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react"
+
 import Lorem from "./Lorem.jsx"
-import "./App.css"
+import Navbar from "./Navbar.jsx"
+import About from "./About.jsx"
+import Arrow from "./Arrow.jsx"
+
+import "./styles/App.css"
 
 export default function App() {
 	return (
 		<div className="responsive-container">
+			<div id="about"></div>
+			<Navbar />
+			<div className="spacer"></div>
+			<About />
 
+			<a className="resume-button" target="_blank" href="#">
+				My Resume
+				<Arrow className="arrow-svg-resume" />
+			</a>
 		</div>
 	)
 }

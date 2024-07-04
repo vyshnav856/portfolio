8d7933d7bb2d62eb26f435adf3cc8d07f31aec85
Vyshnav R <vyshnavr856@gmail.com>
2024-07-04 18:53:26 +0530

    initial vite setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Lorem from "./Lorem.jsx"
+import "./App.css"
+
+export default function App() {
+	return (
+		<div className="responsive-container">
+
+		</div>
+	)
+}
